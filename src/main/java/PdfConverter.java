1b3790e7b1533d2b95e8d5475d91be5b5670ef0e
Guilherme Thadeu <guilherme.horta@flarconsulting.com.br>
2022-06-14 13:37:21 -0300

    PdfTest

--- src/main/java/PdfConverter.java
+++ src/main/java/PdfConverter.java
@@ -6,6 +6,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
@@ -19,7 +22,28 @@ public class PdfConverter {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
 
-            Document doc = builder.parse("C:\\Projetos/senhas.txt");
+            Document doc = builder.parse("C:\\Projetos/Pessoas.xml");
+
+            NodeList listaDePessoas = doc.getElementsByTagName("pessoa");
+
+            int tamanhoLista = listaDePessoas.getLength();
+
+            for(int i = 0; i < tamanhoLista; i++){
+
+                Node noPessoa = listaDePessoas.item(i);
+
+                if(noPessoa.getNodeType() == Node.ELEMENT_NODE)
+                {
+
+                    Element elementoPessoa = (Element) noPessoa;
+
+                    String id = elementoPessoa.getAttribute("id");
+
+                    System.out.println("ID = " + id);
+
+                }
+
+            }
 
         } catch (ParserConfigurationException ex) {
             Logger.getLogger(PdfConverter.class.getName()).log(Level.SEVERE,null, ex);
